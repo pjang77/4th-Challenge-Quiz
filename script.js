25c30ca8e5e1b2f520b5530026ec23352f4f9f89
Ha Young Jang <pauljjang77@gmail.com>
2023-07-22 14:52:34 -0400

    updated html/added outline in JS

---
+++ script.js
@@ -0,0 +1,19 @@
+//global variables : time, 
+let startTime = 120000; 
+const clockEl = document.querySelector("#time")
+console.log(clockEl)
+
+//start timer 
+
+//load first question
+
+//if question wrong subtract time
+
+//if question correct don't subtract time
+
+//any button clicked goes to question 2
+
+//if last question display game over 
+
+
+
